e4d7ee6140db77b4fff9260c92ba17107be97843
johnnyho <johnnyhohohohohoho@gmail.com>
2022-12-31 18:23:12 +0800

    grant permission to nodes

--- terraform/resources/eks/cluster.tf
+++ terraform/resources/eks/cluster.tf
@@ -73,3 +73,15 @@ resource "local_file" "kubeconfig" {
     content = module.eks-kubeconfig.kubeconfig
     filename = "kubeconfig_${local.cluster_name}"
 }
+
+resource "aws_iam_policy" "worker_policy" {
+    name = "worker-policy"
+    description = "worker policy for the ALB Ingress"
+    policy = file("iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "additional" {
+    for_each = module.eks.eks_managed_node_groups
+    policy_arn = aws_iam_policy.worker_policy.arn
+    role = each.value.iam_role_name
+}
